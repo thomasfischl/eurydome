5ed034cd9cbd80e942564f91a78fbe6a8be78617
thomasfischl <fischl.thomas@gmail.com>
2015-02-02 21:46:23 +0100

    Improve Navigationbar: Use dropdown menu to reduce the number of visible entries

--- eurydome-frontend/lib/component/navigation_component.dart
+++ eurydome-frontend/lib/component/navigation_component.dart
@@ -1,11 +1,18 @@
 library NavigationComponentLibrary;
 
 import 'package:angular/angular.dart';
+import 'dart:html';
 
-@Component(
-    selector: 'navigation',
-    templateUrl: 'navigation_component.html',
-    useShadowDom: false)
+@Component(selector: 'navigation', templateUrl: 'navigation_component.html', useShadowDom: false)
 class NavigationComponent {
-  
+
+  void openSettings() {
+    var element = querySelector('#dropdown');
+    if (element.classes.contains('open')) {
+      element.classes.remove('open');
+    } else {
+      element.classes.add('open');
+    }
+  }
+
 }
